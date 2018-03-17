b2340d490906fb50b9770dbb3e18a3ace50de2a3
Javi <Javi@JAVINEW.home>
2018-03-17 13:54:44 +0100

    Menu compleado (ya lo cambiaremos mas adelante), pero la base mas o menos ya esta

--- public_html/js/menu.js
+++ public_html/js/menu.js
@@ -1,14 +1,35 @@
 var bolaDerecha;
 var bolaIzquierda;
 var fuego=false;
+WebFontConfig = {
+    //  The Google Fonts we want to load (specify as many as you like in the array)
+    google: {
+      families: ['Comfortaa']
+    }
+
+};
 var menu ={
     preload: function(){
+        game.add.plugin(PhaserInput.Plugin);
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         //this.scale.pageAlignHorizontally = true;
         this.scale.pageAlignVertically = true;
         //this.scale.setScreenSize(true);
-        game.load.image('jugador', 'assets/player/player1.png');
-        game.load.image('botonPlay', 'assets/menu/btnPlay.png');
+        game.load.image('bIzquierda', 'assets/menu/bolasColores/bola5_grande.png');
+        game.load.image('bDerecha', 'assets/menu/bolasColores/bola8_grande.png');
+        game.load.image('strikeballName', 'assets/menu/strikeballName.png');
+        game.load.image('v2Name', 'assets/menu/v2Name.png');
+        game.load.image('botonVerde', 'assets/menu/botonVerde.png');
+        game.load.image('bola1', 'assets/menu/bolasColores/bola1.png');
+        game.load.image('bola2', 'assets/menu/bolasColores/bola2.png');
+        game.load.image('bola3', 'assets/menu/bolasColores/bola3.png');
+        game.load.image('bola4', 'assets/menu/bolasColores/bola4.png');
+        game.load.image('bola5', 'assets/menu/bolasColores/bola5.png');
+        game.load.image('bola6', 'assets/menu/bolasColores/bola6.png');
+        game.load.image('bola7', 'assets/menu/bolasColores/bola7.png');
+        game.load.image('bola8', 'assets/menu/bolasColores/bola8.png');
+        game.load.image('bola9', 'assets/menu/bolasColores/bola9.png');
+        game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
         
     },
     create: function(){
@@ -77,16 +98,16 @@ var menu ={
         game.physics.startSystem(Phaser.Physics.P2JS);
         game.physics.p2.restitution = 0.9;
         
-        bolaDerecha = game.add.sprite(750, 50, 'jugador');
-        bolaDerecha.scale.setTo(2, 2);
+        bolaDerecha = game.add.sprite(750, 50, 'bDerecha');
+        bolaDerecha.scale.setTo(0.5, 0.5);
         game.physics.p2.enable(bolaDerecha, false);
-        bolaDerecha.body.setCircle(48);
+        bolaDerecha.body.setCircle(55);
         
-        bolaIzquierda = game.add.sprite(50, 550, 'jugador');
+        bolaIzquierda = game.add.sprite(50, 550, 'bIzquierda');
         bolaIzquierda.smoothed = false;
-        bolaIzquierda.scale.setTo(2, 2);
+        bolaIzquierda.scale.setTo(0.5, 0.5);
         game.physics.p2.enable(bolaIzquierda, false);
-        bolaIzquierda.body.setCircle(48);
+        bolaIzquierda.body.setCircle(55);
         
         bolaIzquierda.body.createBodyCallback(bolaDerecha, choqueBolas, this);
         game.physics.p2.setImpactEvents(true);
@@ -108,14 +129,86 @@ function botonPlay(){
 }
 
 function choqueBolas(derecha,izquierda){
-    izquierda.sprite.body.force.x+=13600*4;
-    izquierda.sprite.body.force.y+=-10400*4;
-        
-    derecha.sprite.body.force.x+=-13600*4;
-    derecha.sprite.body.force.y+=10400*4;
+    bolasAlteatorias();
+
+    bolaDerecha.destroy();
+    bolaIzquierda.destroy();
     
     sprite.visible=true;
     fuego=true;
     
-    botonplay = game.add.button(300, 250, 'botonPlay', botonPlay, this, 2, 1, 0);
+    var striballname = game.add.sprite(100, -50, 'strikeballName');
+    var tween = game.add.tween(striballname).to( { y: 40 }, 3000, Phaser.Easing.Bounce.Out, true);
+    
+    var v2name = game.add.sprite(100, 650, 'v2Name');
+    var tween = game.add.tween(v2name).to( { y: 130, x:340 }, 3000, "Sine.easeInOut", true);
+    game.add.tween(v2name).to({angle: 360}, 2400, Phaser.Easing.Cubic.In, true);
+    
+    var inputGrupo = game.add.group();
+    var username = game.add.inputField(330, 290, {
+        font: '18px Arial',
+        fill: '#212121',
+        fontWeight: 'bold',
+        width: 150,
+        padding: 8,
+        borderWidth: 1,
+        borderColor: '#000',
+        borderRadius: 6,
+        placeHolder: 'Username',
+        type: PhaserInput.InputType.text
+    });
+    inputGrupo.add(username);
+    var password = game.add.inputField(330, 335, {
+        font: '18px Arial',
+        fill: '#212121',
+        fontWeight: 'bold',
+        width: 150,
+        padding: 8,
+        borderWidth: 1,
+        borderColor: '#000',
+        borderRadius: 6,
+        placeHolder: 'Password',
+        type: PhaserInput.InputType.password
+    });
+    inputGrupo.add(password);
+    inputGrupo.alpha = 0;
+    var botonLogin= game.add.button(330, 380, 'botonVerde', botonPlay, this, 2, 1, 0);
+    botonLogin.scale.setTo(0.8, 0.8);
+    inputGrupo.add(botonLogin);
+    
+    game.add.tween(inputGrupo).to( { alpha: 1 }, 3500, Phaser.Easing.Linear.None, true, 0, 1000, true);
+    
+    text = game.add.text(360, 387, "LOG IN");
+    //text.anchor.setTo(0.5);
+
+    text.font = 'Comfortaa';
+    text.fontSize = 25;
+    text.fill = '#ffffff';
+    
+}
+
+function bolasAlteatorias(){
+    for (var i = 0; i < 50; i++) {
+        var n= Math.floor(Math.random() * 9) + 1 ;
+        var bolaMini= game.add.sprite(400, 300, ('bola'+n));
+        game.physics.p2.enable(bolaMini, false);
+        var x= Math.floor(Math.random() * 130000) + 80000 ;
+        var y= Math.floor(Math.random() * 130000) + 80000 ;
+        var aleatorio=(Math.floor(Math.random() * 4) + 1);
+        if (aleatorio===1){
+            bolaMini.body.force.x=-x;
+            bolaMini.body.force.y=+y;
+        }else if (aleatorio===2){
+            bolaMini.body.force.x=+x;
+            bolaMini.body.force.y=-y;
+        }else if (aleatorio===3){
+            bolaMini.body.force.x=+x;
+            bolaMini.body.force.y=+y;
+        }else{
+            bolaMini.body.force.x=-x;
+            bolaMini.body.force.y=-y;
+        }
+        
+    }
+    
 }
